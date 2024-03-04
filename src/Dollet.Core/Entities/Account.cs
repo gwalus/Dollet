aa70fe418b3c659929fccbae5d2504b5f6b13ec4
gwalus <gwalus1404@gmail.com>
2024-03-04 19:54:56 +0100

    add account entity

---
+++ src/Dollet.Core/Entities/Account.cs
@@ -0,0 +1,10 @@
+﻿namespace Dollet.Core.Entities
+{
+    public class Account : BaseEntity
+    {
+        public required string Name { get; set; }
+        public required string Icon { get; set; }
+        public required string Color { get; set; }
+        public bool IsHidden { get; set; } = false;
+    }
+}
---
+++ src/Dollet.Core/Entities/BaseEntity.cs
@@ -0,0 +1,7 @@
+﻿namespace Dollet.Core.Entities
+{
+    public class BaseEntity
+    {
+        public int Id { get; set; }
+    }
+}
