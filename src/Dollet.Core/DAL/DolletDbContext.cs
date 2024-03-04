3912cbd35423845cbc339a929f94d01cb4a45ca5
gwalus <gwalus1404@gmail.com>
2024-03-04 19:58:12 +0100

    add DolletDbContext

---
+++ src/Dollet.Core/DAL/DolletDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Dollet.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Dollet.Core.DAL
+{
+    public class DolletDbContext(DbContextOptions options) : DbContext(options)
+    {
+        public DbSet<Account> Accounts { get; set; }
+        public DbSet<Expense> Expenses { get; set; }
+        public DbSet<Income> Incomes { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Currency> Currencies { get; set; }
+    }
+}
