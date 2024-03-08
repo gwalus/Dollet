ddba2433972434bcd96ce76b0d9c9a3e441fee99
gwalus <gwalus1404@gmail.com>
2024-03-08 21:58:33 +0100

    add first character converter

---
+++ src/Dollet.Presentation/Maui/Conventers/FirstCharacterConverter.cs
@@ -0,0 +1,20 @@
+﻿using System.Globalization;
+
+namespace Dollet.Conventers
+{
+    internal class FirstCharacterConverter : IValueConverter
+    {
+        public object? Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null || value is not string)
+                return null;
+
+            return ((string)value).Length > 0 ? ((string)value).ToUpperInvariant()[0] : string.Empty;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new InvalidOperationException();
+        }
+    }
+}
