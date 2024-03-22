f92fd001d7ddd0b1ddbb406e924880961e63f91e
Grzegorz Waluś <gwalus1404@gmail.com>
2024-03-22 17:52:18 +0100

    understand how collection view groupable works

--- src/Dollet.Core/Entities/Entry.cs
+++ src/Dollet.Core/Entities/Entry.cs
@@ -2,12 +2,19 @@
 {
     public class Entry : BaseEntity
     {
-        public required decimal Amount { get; set; }
-        public required Currency Currency { get; set; }
-        public required Account Account { get; set; }
-        public required Category Category { get; set; }
-        public required DateTime Date { get; set; }
+        public decimal Amount { get; set; }
+        public Currency Currency { get; set; }
+        public Account Account { get; set; }
+        public Category Category { get; set; }
+        public DateTime Date { get; set; }
         public string? Comment { get; set; }
+
+        //public required decimal Amount { get; set; }
+        //public required Currency Currency { get; set; }
+        //public required Account Account { get; set; }
+        //public required Category Category { get; set; }
+        //public required DateTime Date { get; set; }
+        //public string? Comment { get; set; }
 
         // Photos?
     }
--- src/Dollet.Presentation/Maui/ViewModels/Transactions/ExpensesPageViewModel.cs
+++ src/Dollet.Presentation/Maui/ViewModels/Transactions/ExpensesPageViewModel.cs
@@ -1,8 +1,93 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using Dollet.Core.Entities;
 
 namespace Dollet.ViewModels.Transactions
 {
     public class ExpensesPageViewModel : ObservableObject
     {
+        public List<ExpensesGroup> Expenses { get; private set; } = [];
+
+        public ExpensesPageViewModel()
+        {
+            Expenses.Add(new ExpensesGroup("Groceries",
+            [
+                new() 
+                {
+                    Amount = 10,
+                    Date = DateTime.Now,
+                    Comment = "Biedronka"
+                },
+                new() 
+                {
+                    Amount = 20,
+                    Date = DateTime.Now,
+                    Comment = "Lidl"
+                }
+            ]));
+
+            Expenses.Add(new ExpensesGroup("Activity",
+            [
+                new()
+                {
+                    Amount = 30,
+                    Date = DateTime.Now.AddDays(-1),
+                    Comment = "Krykiet"
+                },
+                new()
+                {
+                    Amount = 40,
+                    Date = DateTime.Now.AddDays(-1),
+                    Comment = "Tenis"
+                },
+                new() 
+                {
+                    Amount = 50,
+                    Date = DateTime.Now.AddDays(-1),
+                    Comment = "Buty do biegania"
+                }
+            ]));
+
+            Expenses.Add(new ExpensesGroup("House",
+            [
+                new()
+                {
+                    Amount = 30,
+                    Date = DateTime.Now.AddDays(-1),
+                    Comment = "Nowa szafka"
+                },
+                new()
+                {
+                    Amount = 40,
+                    Date = DateTime.Now.AddDays(-1),
+                    Comment = "Koc"
+                }
+            ]));
+
+            Expenses.Add(new ExpensesGroup("Other",
+            [
+                new()
+                {
+                    Amount = 10,
+                    Date = DateTime.Now,
+                    Comment = "Cos tam A"
+                },
+                new()
+                {
+                    Amount = 20,
+                    Date = DateTime.Now,
+                    Comment = "Cos tam B"
+                }
+            ]));
+        }
+    }
+
+    public class ExpensesGroup : List<Expense>
+    {
+        public string CategoryName { get; private set; }
+
+        public ExpensesGroup(string categoryName, List<Expense> expenses) : base(expenses)
+        {
+            CategoryName = categoryName;
+        }
     }
 }
