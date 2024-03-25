78a840f53a7e1f5096eaa6f8f2c3f2e0c68149a5
gwalus <gwalus1404@gmail.com>
2024-03-25 20:42:24 +0100

    create category repository

---
+++ src/Dollet.Infrastructure/DAL/Repositories/CategoryRepository.cs
@@ -0,0 +1,22 @@
+﻿using Dollet.Core.Abstractions.Repositories;
+using Dollet.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Dollet.Infrastructure.DAL.Repositories
+{
+    internal class CategoryRepository(DolletDbContext dbContext) : ICategoryRepository
+    {
+        private readonly DolletDbContext _dbContext = dbContext;
+
+        public async Task<bool> AddAsync(Category category)
+        {
+            await _dbContext.Categories.AddAsync(category);
+            return await _dbContext.SaveChangesAsync() > 0;
+        }
+
+        public async Task<IEnumerable<Category>> GetAllAsync()
+        {
+            return await _dbContext.Categories.ToListAsync();
+        }
+    }
+}
