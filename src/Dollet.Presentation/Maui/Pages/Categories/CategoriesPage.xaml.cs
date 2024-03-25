d62541c85aea6ffc2387765043d323a84a88ead1
gwalus <gwalus1404@gmail.com>
2024-03-25 20:44:33 +0100

    handle getting categories in category page view model

--- src/Dollet.Presentation/Maui/Pages/Categories/CategoriesPage.xaml.cs
+++ src/Dollet.Presentation/Maui/Pages/Categories/CategoriesPage.xaml.cs
@@ -1,14 +1,13 @@
-using Dollet.Helpers;
 using Dollet.ViewModels.Categories;
 
 namespace Dollet.Pages.Categories;
 
 public partial class CategoriesPage : ContentPage
 {
-	public CategoriesPage()
+	public CategoriesPage(CategoriesPageViewModel viewModel)
 	{
 		InitializeComponent();
 
-        BindingContext = ServiceProviderHelper.GetService<CategoriesPageViewModel>();
+		BindingContext = viewModel;
     }
 }
--- src/Dollet.Presentation/Maui/ViewModels/Categories/CategoriesPageViewModel.cs
+++ src/Dollet.Presentation/Maui/ViewModels/Categories/CategoriesPageViewModel.cs
@@ -1,28 +1,26 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
+﻿using CommunityToolkit.Mvvm.Input;
+using Dollet.Core.Abstractions.Repositories;
 using Dollet.Core.Entities;
-using Dollet.Helpers.Fonts;
-using System.Collections.ObjectModel;
+using MvvmHelpers;
 
 namespace Dollet.ViewModels.Categories
 {
-    public partial class CategoriesPageViewModel : ObservableObject
+    public partial class CategoriesPageViewModel(ICategoryRepository categoryRepository) : ObservableObject
     {
-        public ObservableCollection<Category> Categories { get; set; }
+        private readonly ICategoryRepository _categoryRepository = categoryRepository;
 
-        public CategoriesPageViewModel()
+        public ObservableRangeCollection<Category> Categories { get; } = [];
+
+        [RelayCommand]
+        async Task Appearing()
         {
-            Categories =
-            [
-                new Category { Name = "Groceries", Icon = MaterialDesignIcons.Local_grocery_store, Color = "#d2b7b7" },
-                new Category { Name = "Activity", Icon = MaterialDesignIcons.Sports_baseball, Color = "#a76e6e" },
-                new Category { Name = "Entertaiments", Icon = MaterialDesignIcons.Sports_bar, Color = "#88af95" },
-                new Category { Name = "Restaurants", Icon = MaterialDesignIcons.Restaurant, Color = "#819a78" },
-                new Category { Name = "House", Icon = MaterialDesignIcons.House, Color = "#7782b0" },
-                new Category { Name = "Education", Icon = MaterialDesignIcons.Science, Color = "#606a9f" },
-                new Category { Name = "Subscriptions", Icon = MaterialDesignIcons.Subscriptions, Color = "#e39e83" },
-                new Category { Name = "Cafes", Icon = MaterialDesignIcons.Local_cafe, Color = "#819a78" },
-                new Category { Name = "Other", Icon = MaterialDesignIcons.Done_all, Color = "#d2b7b7" }
-            ];
+            Categories.AddRange(await _categoryRepository.GetAllAsync());
+        }
+
+        [RelayCommand]
+        void Disappearing()
+        {
+            Categories.Clear();
         }
     }
 }
